5654e72933c2e587e5ce535d65793e7d6643c569
Reece <rfarnworth92@gmail.com>
2019-12-01 20:50:05 +0000

    nav and header complete

---
+++ resources/app.js
@@ -0,0 +1,30 @@
+//find the navc
+const nav = document.querySelector(".nav");
+
+//on scroll add the new class
+window.addEventListener("scroll", () => {
+    let top = window.scrollY;
+    
+    if(top != 0){
+        nav.classList.add("navmve");
+    } else {
+        nav.classList.remove("navmve");
+    }
+
+    console.log(top);
+})
+
+
+//parallax the imgs on the header
+function parallax(e, d, s, r) {
+    const item = document.querySelector(e);
+    item.style.transform = `translateY(${d * s}px) rotate(${r}deg)`;
+}
+
+window.addEventListener("scroll", () => {
+    parallax("#header", window.scrollY, 1);
+    parallax(".pc", window.scrollY, 2.5, 25);
+    parallax(".coffee", window.scrollY, -0.7, -20);
+    parallax(".tablet", window.scrollY, 5, 40);
+    parallax(".plant", window.scrollY, -2, 0);
+});
